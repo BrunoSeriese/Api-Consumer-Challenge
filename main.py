b58722be316a64ea814287f74aef621e76f6147b
Seriese Bruno <bruno.seriese@swisscom.com>
2024-06-11 18:02:10 +0200

    update main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import httpx
+
+hosts = [
+    'node1.example.com',
+    'node2.example.com',
+    'node3.example.com'
+]
+
+
+
+
+def createGroup():
+    print("create group")
+def delete_group():
+    print("delete group")
+def get_group():
+    print('get group')
